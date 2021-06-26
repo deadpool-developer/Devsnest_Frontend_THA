9af3c48ec0023bafda619e857d9ae6dd87a4864c
Aditya Saini <adibhu025@gmail.com>
2021-06-26 21:45:46 +0530

    Day11 commit

---
+++ Day11/question.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question:
+        " What is the only thing that computers understand?",
+        option1: "Machine Code",
+        option2: "Low Level Languages",
+        option3: "High Level Languages",
+        option4: "Algorithms",
+        answer: "Machine Code",
+    },
+    {
+        question:
+        " Before a computer can understand a program it must be...",
+        option1: "Translated into its machine code.",
+        option2: "Translated into a high level language.",
+        option3: "Translated into a low level language.",
+        option4: "None of the",
+        answer: "Translated into its machine code.",
+    },
+    {
+        question:
+        " A list of instructions that enable a computer to perform a specific task is a...",
+        option1: "Computer Program",
+        option2: "Algorithm",
+        option3: "Machine Code",
+        option4: "Binary Code",
+        answer: "Computer Program",
+    },
+    {
+        question:
+        " Resolving errors in a program is known as...",
+        option1: "Debugging",
+        option2: "Refixing",
+        option3: "Error Checking",
+        option4: "Problem Solving",
+        answer: "Debugging",
+    },
+    {
+        question:
+        " Which of the following is not a high level programming language?",
+        option1: "Assembly",
+        option2: "C++",
+        option3: "Java",
+        option4: "Python",
+        answer: "Assembly",
+    },
+];
+
+export default questions;
